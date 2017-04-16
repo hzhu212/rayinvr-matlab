7c0c4a236fde7cd3aa591b21e11f07215de704a7
hzhu212 <zhuhe212@163.com>
2017-04-16 17:30:25 +0800

    fixed fun_dstep

--- functions/trc.f/fun_dstep.m
+++ functions/trc.f/fun_dstep.m
@@ -3,17 +3,13 @@
 % called by: fun_trace;
 % call: fun_vel; done.
 
-% fortran function
-
 function [dstep] = fun_dstep(x,z)
 % calculate ray step length - step length is a function of the
 % absolute value of the velocity divided by the sum of the
 % absolute values of the velocity gradients in the x and z
 % directions
-
-	% global file_rayinvr_par file_rayinvr_com;
-	% run(file_rayinvr_par);
-	% run(file_rayinvr_com);
+% 计算射线步长。某点处的射线步长等于 该点速度除以 x 方向速度梯度绝对值与 z 方向速度梯度
+% 绝度值之和，最后乘以某个固定系数 step_。即：【 dstep = A * v / (|vx| + |vz|) 】
 
 	global c iblk layer smax smin step_;
 
