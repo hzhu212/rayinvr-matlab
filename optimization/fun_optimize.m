507af30ad0a7729aca10657fa57fbb785fbcb402
haley <zhuhe212@163.com>
2017-09-28 01:18:16 +0800

    添加对基因算法过程的记录和图示

--- optimization/fun_optimize.m
+++ optimization/fun_optimize.m
@@ -5,17 +5,18 @@ function [x,fval] = fun_optimize(mainOptions)
 	pois = fun_init_pois(file_rin);
 	% 只对前若干层应用基因算法，提高计算效率
 	nvarsMax = length(pois);
-	nvars = min(nvarsMax, 2);
+	nvar = min(nvarsMax, 2);
 
 	mainOptions.isPlot = false;
 	target_fun = fun_partialMain(mainOptions);
 	% [x, fval] = fminunc(target_fun, pois);
 
 	% default options:
-	% Generations: 100 * nvars
-	% PopulationSize: {50} when numberOfVariables <= 5, {200} otherwise | {min(max(10*nvars,40),100)} for mixed-integer problems
-	options = gaoptimset('Generations', 10, 'PopulationSize', 20);
-	[x, fval] = ga(target_fun, nvars, [],[],[],[],zeros(nvars,1),ones(nvars,1)*0.5,[],options);
+	% Generations: 100 * nvar
+	% PopulationSize: {50} when numberOfVariables <= 5, {200} otherwise | {min(max(10*nvar,40),100)} for mixed-integer problems
+	% options = gaoptimset('Generations', 10, 'PopulationSize', 20);
+	options = optimoptions('ga', 'OutputFcn', @gaoutfun, 'Generations', 10, 'PopulationSize', 20);
+	[x, fval] = ga(target_fun, nvar, [],[],[],[],zeros(nvar,1),ones(nvar,1)*0.5,[],options);
 end
 
 function func = fun_partialMain(mainOptions)
@@ -26,4 +27,73 @@ function func = fun_partialMain(mainOptions)
 	end
 
 	func = @wrapper;
+end
+
+% output function for ga
+function [state,options,optchanged] = gaoutfun(options,state,flag)
+	persistent hf ax history h x y z;
+	nGen = options.Generations;
+	nPop = options.PopulationSize;
+	curGen = state.Generation + 1;
+	optchanged = false;
+	switch flag
+	    case 'init'
+	        hf = figure;
+	        ax = gca;
+	        ax.XLim = [0 nGen];
+	        ax.YLim = [0 nPop];
+	        xlabel('X - Generation');
+	        ylabel('Y - Population');
+	        zlabel('Z - Score');
+	        % x = curGen*ones(1,nPop);
+	        % y = 1:nPop;
+	        % z = state.Score';
+	        % h = scatter3(ax, x, y, z);
+	        y = zeros(nPop, nGen);
+	        y(:,curGen) = state.Score;
+	        h = bar3(ax, y);
+	        pause(0.1);
+	        history(:,:,1) = state.Population;
+	        assignin('base','gapopulationhistory',history);
+	    case 'iter'
+	        % Update the history every 10 generations.
+	        if rem(state.Generation,10) == 0
+	            ss = size(history,3);
+	            history(:,:,ss+1) = state.Population;
+	            assignin('base','gapopulationhistory',history);
+	        end
+	        % % Find the best objective function, and stop if it is low.
+	        % ibest = state.Best(end);
+	        % ibest = find(state.Score == ibest,1,'last');
+	        % bestx = state.Population(ibest,:);
+	        % bestf = gaintobj(bestx);
+	        % if bestf <= 0.1
+	        %     state.StopFlag = 'y';
+	        %     disp('Got below 0.1')
+	        % end
+	        % Update the plot.
+	        % [x, y] = meshgrid(1:curGen, 1:nPop);
+	        % z = [h.ZData', state.Score];
+	        % h = scatter3(ax, x, y, z);
+	        % y = [y, state.Score];
+	        % h = bar3(ax, y);
+	        y(:,curGen) = state.Score;
+	        % refreshdata(h, 'caller');
+	        h = bar3(ax, y);
+	        figure(hf);
+	        pause(0.1);
+	        % Update the fraction of mutation and crossover after 25 generations.
+	        % if state.Generation == 25
+	        %     options.CrossoverFraction = 0.8;
+	        %     optchanged = true;
+	        % end
+	    case 'done'
+	        % Include the final population in the history.
+	        ss = size(history,3);
+	        history(:,:,ss+1) = state.Population;
+	        assignin('base','gapopulationhistory',history);
+	        xlabel('X - Generation');
+	        ylabel('Y - Population');
+	        zlabel('Z - Score');
+	    end
 end
