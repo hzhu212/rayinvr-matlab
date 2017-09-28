94d6eea430325535e465429d2b6403b416ff0a51
haley <zhuhe212@163.com>
2017-09-28 11:56:39 +0800

    添加展示基因算法过程的多种绘图方法

--- optimization/fun_optimize.m
+++ optimization/fun_optimize.m
@@ -15,7 +15,8 @@ function [x,fval] = fun_optimize(mainOptions)
 	% Generations: 100 * nvar
 	% PopulationSize: {50} when numberOfVariables <= 5, {200} otherwise | {min(max(10*nvar,40),100)} for mixed-integer problems
 	% options = gaoptimset('Generations', 10, 'PopulationSize', 20);
-	options = optimoptions('ga', 'OutputFcn', @gaoutfun, 'Generations', 10, 'PopulationSize', 20);
+	gaoutfun = @gaoutfun_bar3;
+	options = optimoptions('ga', 'OutputFcn', gaoutfun, 'Generations', 3, 'PopulationSize', 2);
 	[x, fval] = ga(target_fun, nvar, [],[],[],[],zeros(nvar,1),ones(nvar,1)*0.5,[],options);
 end
 
@@ -29,8 +30,124 @@ function func = fun_partialMain(mainOptions)
 	func = @wrapper;
 end
 
-% output function for ga
-function [state,options,optchanged] = gaoutfun(options,state,flag)
+%% output function for ga
+
+function [state,options,optchanged] = gaoutfun_stem3(options,state,flag)
+	persistent hf history h z;
+	nGen = options.Generations;
+	nPop = options.PopulationSize;
+	curGen = state.Generation;
+	optchanged = false;
+	switch flag
+	    case 'init'
+	        hf = figure;
+	        ax = gca;
+	        [x, y] = meshgrid(0:nGen, 1:nPop);
+	        z = zeros(nPop, nGen+1);
+	        x = x(:)'; y = y(:)'; z = z(:)';
+	        z(nPop*curGen+1:nPop*(curGen+1)) = state.Score';
+	        h = stem3(ax, x, y, z, 'Marker','s','MarkerEdgeColor','m','MarkerFaceColor','g');
+	        h.ZDataSource = 'z';
+	        ax.XTick = 0:nGen;
+	        ax.YTick = 1:nPop;
+	        xlabel(ax, 'X - Generation');
+	        ylabel(ax, 'Y - Population');
+	        zlabel(ax, 'Z - Score');
+	        pause(0.1);
+	        history(:,:,1) = state.Population;
+	        assignin('base','gapopulationhistory',history);
+	    case 'iter'
+	        % Update the history every 10 generations.
+            ss = size(history,3);
+            history(:,:,ss+1) = state.Population;
+            assignin('base','gapopulationhistory',history);
+	        % Update the plot.
+	        z(nPop*curGen+1:nPop*(curGen+1)) = state.Score';
+	        refreshdata(h, 'caller');
+	        figure(hf);
+	        pause(0.1);
+	    case 'done'
+	        % Include the final population in the history.
+	    end
+end
+
+function [state,options,optchanged] = gaoutfun_surf(options,state,flag)
+	persistent hf history h z;
+	nGen = options.Generations;
+	nPop = options.PopulationSize;
+	curGen = state.Generation + 1;
+	optchanged = false;
+	switch flag
+	    case 'init'
+	        hf = figure;
+	        ax = gca;
+	        [x, y] = meshgrid(0:nGen, 1:nPop);
+	        z = zeros(nPop, nGen+1);
+	        z(:,curGen) = state.Score;
+	        h = surf(ax, x, y, z);
+	        h.ZDataSource = 'z';
+	        ax.XTick = 0:nGen;
+	        ax.YTick = 1:nPop;
+	        xlabel(ax, 'X - Generation');
+	        ylabel(ax, 'Y - Population');
+	        zlabel(ax, 'Z - Score');
+	        pause(0.1);
+	        history(:,:,1) = state.Population;
+	        assignin('base','gapopulationhistory',history);
+	    case 'iter'
+	        % Update the history every 10 generations.
+            ss = size(history,3);
+            history(:,:,ss+1) = state.Population;
+            assignin('base','gapopulationhistory',history);
+	        % Update the plot.
+	        z(:,curGen) = state.Score;
+	        refreshdata(h, 'caller');
+	        figure(hf);
+	        pause(0.1);
+	    case 'done'
+	        % Include the final population in the history.
+	    end
+end
+
+function [state,options,optchanged] = gaoutfun_mesh(options,state,flag)
+	persistent hf history h z;
+	nGen = options.Generations;
+	nPop = options.PopulationSize;
+	curGen = state.Generation + 1;
+	optchanged = false;
+	switch flag
+	    case 'init'
+	        hf = figure;
+	        ax = gca;
+	        [x, y] = meshgrid(0:nGen, 1:nPop);
+	        z = zeros(nPop, nGen+1);
+	        z(:,curGen) = state.Score;
+	        h = mesh(ax, x, y, z);
+	        h.ZDataSource = 'z';
+	        ax.XTick = 0:nGen;
+	        ax.YTick = 1:nPop;
+	        xlabel(ax, 'X - Generation');
+	        ylabel(ax, 'Y - Population');
+	        zlabel(ax, 'Z - Score');
+	        pause(0.1);
+	        history(:,:,1) = state.Population;
+	        assignin('base','gapopulationhistory',history);
+	    case 'iter'
+	        % Update the history every 10 generations.
+            ss = size(history,3);
+            history(:,:,ss+1) = state.Population;
+            assignin('base','gapopulationhistory',history);
+	        % Update the plot.
+	        z(:,curGen) = state.Score;
+	        refreshdata(h, 'caller');
+	        figure(hf);
+	        pause(0.1);
+	    case 'done'
+	        % Include the final population in the history.
+	    end
+end
+
+function [state,options,optchanged] = gaoutfun_bar3(options,state,flag)
 	persistent hf ax history h x y z;
 	nGen = options.Generations;
 	nPop = options.PopulationSize;
@@ -40,60 +157,30 @@ function [state,options,optchanged] = gaoutfun(options,state,flag)
 	    case 'init'
 	        hf = figure;
 	        ax = gca;
-	        ax.XLim = [0 nGen];
-	        ax.YLim = [0 nPop];
-	        xlabel('X - Generation');
-	        ylabel('Y - Population');
-	        zlabel('Z - Score');
-	        % x = curGen*ones(1,nPop);
-	        % y = 1:nPop;
-	        % z = state.Score';
-	        % h = scatter3(ax, x, y, z);
-	        y = zeros(nPop, nGen);
+	        y = zeros(nPop, nGen+1);
 	        y(:,curGen) = state.Score;
 	        h = bar3(ax, y);
+	        ax.XLim = [0 nGen];
+	        ax.YLim = [0 nPop];
+	        xlabel(ax, 'X - Generation');
+	        ylabel(ax, 'Y - Population');
+	        zlabel(ax, 'Z - Score');
 	        pause(0.1);
 	        history(:,:,1) = state.Population;
 	        assignin('base','gapopulationhistory',history);
 	    case 'iter'
 	        % Update the history every 10 generations.
-	        if rem(state.Generation,10) == 0
-	            ss = size(history,3);
-	            history(:,:,ss+1) = state.Population;
-	            assignin('base','gapopulationhistory',history);
-	        end
-	        % % Find the best objective function, and stop if it is low.
-	        % ibest = state.Best(end);
-	        % ibest = find(state.Score == ibest,1,'last');
-	        % bestx = state.Population(ibest,:);
-	        % bestf = gaintobj(bestx);
-	        % if bestf <= 0.1
-	        %     state.StopFlag = 'y';
-	        %     disp('Got below 0.1')
-	        % end
-	        % Update the plot.
-	        % [x, y] = meshgrid(1:curGen, 1:nPop);
-	        % z = [h.ZData', state.Score];
-	        % h = scatter3(ax, x, y, z);
-	        % y = [y, state.Score];
-	        % h = bar3(ax, y);
+            ss = size(history,3);
+            history(:,:,ss+1) = state.Population;
+            assignin('base','gapopulationhistory',history);
 	        y(:,curGen) = state.Score;
-	        % refreshdata(h, 'caller');
 	        h = bar3(ax, y);
 	        figure(hf);
 	        pause(0.1);
-	        % Update the fraction of mutation and crossover after 25 generations.
-	        % if state.Generation == 25
-	        %     options.CrossoverFraction = 0.8;
-	        %     optchanged = true;
-	        % end
 	    case 'done'
 	        % Include the final population in the history.
-	        ss = size(history,3);
-	        history(:,:,ss+1) = state.Population;
-	        assignin('base','gapopulationhistory',history);
-	        xlabel('X - Generation');
-	        ylabel('Y - Population');
-	        zlabel('Z - Score');
+	        xlabel(ax, 'X - Generation');
+	        ylabel(ax, 'Y - Population');
+	        zlabel(ax, 'Z - Score');
 	    end
 end
