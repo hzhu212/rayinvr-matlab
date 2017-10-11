f4d6456d7181a0354044462aa9e80573b23675a2
haley <zhuhe212@163.com>
2017-10-11 13:30:53 +0800

    修复 rkdumb 计算 ode 数值过大越界导致主程序报错的 bug（越界问题不可避免，但主程序仍继续进行）

--- functions/runge-kutta/fun_odezfi.m
+++ functions/runge-kutta/fun_odezfi.m
@@ -1,7 +1,7 @@
 % rngkta.f
 % old: [~,~,f] = fun_odezfi(x,y,f)
 % [f]
-% called by: fun_trace -> fun_rkdump;
+% called by: fun_trace -> fun_rkdumb;
 % call: none.
 
 function [f] = fun_odezfi(x,y, btan,c,factan,iblk,layer,mtan)
@@ -11,17 +11,18 @@ function [f] = fun_odezfi(x,y, btan,c,factan,iblk,layer,mtan)
 	% global btan c factan iblk layer mtan;
 
 	% real y(2),f(2)
-	% f = zeros(1,2);
+	f = zeros(1,2);
 
 	sa = 1.0 .* sign(y(2));
 	n1 = fix(sa.*y(2).*factan) + 1; % int -> fix
 	try
 		f(1) = mtan(n1).*y(2) + sa.*btan(n1);
 	catch e
-		disp('======================');
-		disp('pois is too high(close to 0.5)');
+		% disp(e);
+		% n1 oversize causing mtan index out of range
+		% disp('======================');
+		% fprintf('output gradient of rkdumb is too high.\nplease check pois setting');
 	end
-	% f(1) = mtan(n1).*y(2) + sa.*btan(n1);
 	term1 = c(layer,iblk,3) + c(layer,iblk,4) .* y(1);
 	term2 = y(1).*(c(layer,iblk,1)+c(layer,iblk,2).*y(1)) + term1.*x + c(layer,iblk,5);
 	vxv = (y(1).*(c(layer,iblk,8)+c(layer,iblk,9).*y(1))+c(layer,iblk,10).*x+c(layer,iblk,11)) ./ (term2.*(c(layer,iblk,6).*y(1)+c(layer,iblk,7)));
--- functions/runge-kutta/fun_rkdumb.m
+++ functions/runge-kutta/fun_rkdumb.m
@@ -16,39 +16,31 @@ function [y] = fun_rkdumb(y,x1,x2,derivs, btan,c,factan,iblk,layer,mtan)
 % 返回值: y(1),y(2) 分别为下一个节点的 z 坐标和入射角
 
 	% dimension y(2),dydx(2),yt(2),dyt(2),dym(2)
+	y = y(1:2);
 
 	x = x1;
 	h = x2 - x1;
 	if x+h == x, return; end
-
 	[dydx] = derivs(x,y, btan,c,factan,iblk,layer,mtan);
 
 	hh = h .* 0.5;
 	h6 = h ./ 6.0;
 	xh = x + hh;
-
-	for ii = 1:2 % 11
-		yt(ii) = y(ii) + hh .* dydx(ii);
-	end % 11
-
+	yt = y + hh .* dydx;
 	[dyt] = derivs(xh,yt, btan,c,factan,iblk,layer,mtan);
 
-	for ii = 1:2 % 12
-		yt(ii) = y(ii) + hh .* dyt(ii);
-	end % 12
-
+	yt = y + hh .* dyt;
 	[dym] = derivs(xh,yt, btan,c,factan,iblk,layer,mtan);
 
-	for ii = 1:2 % 13
-		yt(ii) = y(ii) + h .* dym(ii);
-		dym(ii) = dyt(ii) + dym(ii);
-	end % 13
+	yt = y + h .* dym;
+	dym = dyt + dym;
 
+	% if yt(2) > 1000000
+	%     disp('-- fun_rkdumb 4 --');
+	%     disp(yt(2));
+	% end
 	[dyt] = derivs(x+h,yt, btan,c,factan,iblk,layer,mtan);
 
-	for ii = 1:2 % 14
-		y(ii) = y(ii) + h6 .* (dydx(ii)+dyt(ii)+2.0.*dym(ii));
-	end % 14
-	return;
+	y = y + h6 .* (dydx+dyt+2.0.*dym);
 
 end % function end
